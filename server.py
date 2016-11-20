5409ce8cb0de8e4b2d9bf06e194153bc5a171489
Conor <bannonco@tcd.ie>
2016-11-20 18:21:38 +0000

    updated server

--- server.py
+++ server.py
@@ -1,31 +1,64 @@
-from socket import *
-from threading import Thread
-from Queue import Queue
 import sys
+from socket import *
+from Queue import Queue
+from threading import Thread
 
 
-def messages_handler(connect,msg)
+def messages(connect,msg):
 	try:
 		if msg[:13] == "JOIN_CHATROOM":
-			##
+			retmsg="JOINED_CHATROOM: Chatroom\nSERVER_IP:0\nPORT:0\nROOM_REF:1\nJOIN_ID: 0\n"
+			connect.sendall(retmsg)
 		elif msg[:14] =="LEAVE_CHATROOM":
-			##
+			retmsg="LEFT_CHATROOM: Chatroom\nJOIN_ID: 0\n"
+			connect.sendall(retmsg)
 		elif msg[:4] =="CHAT":
-			##
+			retmsg="CHAT: 1\nCLIENT_NAME: Name\nMESSAGE: Hello\n"
+			connect.sendall(retmsg)
 		elif msg[:10] =="DISCONNECT":
-			##
+			retmsg="DISCONNECT: 0\nPORT: 0\nCLIENT_NAME: Name\n"
+			connect.sendall(retmsg)
+			connection.close()
 		else:
-			connect.sendall("Parse error")
-	except Exception as error:
-		errmsg=error.message
+			errmsg="ERROR_CODE:1\nERROR_DESCRIPTION: Parse error\n"
+			connect.sendall(errmsg)
+			
+	except Exception as error:	
+		errmsg="ERROR_CODE:2\nERROR_DESCRIPTION: %s\n"%(error.message)
 		connect.sendall(errmsg)
 
 
-def chat_server(hostname,port_number,numb_of_threads)
+def chat_server(hostname,port_number,numb_of_threads):
 	sock=socket(AF_INET,SOCK_STREAM)
 	sock.setsockopt(SOL_SOCKET,SO_REUSEADDR,1)
+	sock.bind((hostname,port_number))
+	sock.listen(10)
 	pool=Queue(numb_of_threads)
+	threads=[]
 
+	def handle(pool):
+		while True:
+			connect,address=pool.get()
+			msg=connect.recv(2048)
+			messages(connect,msg)
+
+	try:
+		for x in range(numb_of_threads):
+			thread=Thread(target=handle,args=(pool,))
+			thread.daemon=True
+			thread.start()
+			threads.append(thread)
+
+		while True:
+			connect,address=sock.accept()
+			try:
+				pool.put((connect,address),False)
+			except Queue.Full:
+				print("Queue full")
+		sock.close()
+	except(KeyboardInterrupt,SystemExit):
+		sock.close()
+		sys.exit(0)	
 
 
 if __name__ == "__main__":
