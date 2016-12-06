7c73e238d36d8075e44120e4214bcbf2327adb47
Conor Bannon <bannonco@scss.tcd.ie>
2016-12-06 16:42:31 +0000

    server updated

--- chat_room.py
+++ chat_room.py
@@ -5,21 +5,31 @@ import threading
 class ChatRoom:
 
 	def __init__(self,hostname,portnum):
-		self.host=hostname
-		self.port=portnum
+		self.hostname=hostname
+		self.portnum=portnum
 		
 	def joinroom(self,chatrooms,roomname,clientname,roomref,join_id,connect):
-		retmsg="JOINED_CHATROOM: room1\nSERVER_IP:%s\nPORT: 0\nROOM_REF: 1\nJOIN_ID: 0\n"%(serverip)
-		connect.sendall(retmsg)
+		if room_ref not in chatrooms:
+			
+			chatrooms[room_ref]={}
+		if join_id not in chatrooms[room_ref]:
+			print("here")
+			chatrooms[room_ref][join_id]=connect
+			retmsg="JOINED_CHATROOM:%s\nSERVER_IP:%s\nPORT:%s\nROOM_REF:%s\nJOIN_ID:%s\n"%(roomname,self.hostname,self.portnum,roomref,join_id)
+			connect.sendall(retmsg)
 	
-	def leaveroom(self):
-		retmsg="LEFT_CHATROOM: room1\nJOIN_ID: 0\n"
-		connect.sendall(retmsg)
+	def leaveroom(self,chatrooms,room_ref,join_id,clientname,connect):
 	
-	def disconnect(self):
-		retmsg="DISCONNECT: 0\nPORT: 0\nCLIENT_NAME: Name\n"
+		retmsg="LEFT_CHATROOM:%s\nJOIN_ID:%s\n"%(str(roomref),str(join_id))
 		connect.sendall(retmsg)
-		connection.close()
-	def chat(self):
-		retmsg="CHAT: 1\nCLIENT_NAME: Name\nMESSAGE: Hello\n"
+		del chatrooms[room_ref][join_id]
+	
+	def disconnect(self,chatrooms,join_id,clientname,connect):
+		for room in chatrooms.keys():
+			if join_id in chatrooms[join_id]:
+				retmsg="DISCONNECT: 0\nPORT: 0\nCLIENT_NAME: Name\n"
+				connect.sendall(retmsg)
+				del chatrooms[room][join_id]
+	def chat(self,chatrooms,room_ref,join_id,clientname,msg,connect):
+		retmsg="CHAT:%s\nCLIENT_NAME:%s\nMESSAGE:%s\n\n"%(str(room_ref),str(clientname),msg)
 		connect.sendall(retmsg)
--- server.py
+++ server.py
@@ -3,65 +3,81 @@ from socket import *
 from Queue import Queue
 from threading import Thread
 import collections
-from chat_room import ChatRoom
-from random import randint
+#from chat_room import ChatRoom
 
-chat_room=ChatRoom(sys.argv[1],int(sys.argv[2]))
-rooms=collections.OrderedDict()
-active=True
+#chat_room=ChatRoom(sys.argv[1],int(sys.argv[2]))
+#rooms=collections.OrderedDict()
+
 
 def messages(connect,msg):
-	while active
-		try:
-			if msg[:13] == "JOIN_CHATROOM":
-				print("joining chatroom")
-				msgs=msg.split("\n")
-				chatroom=msgs[0].split(":")[1]
-				client_name=msgs[3].split(":")[1]
-				room_ref=
-				join_id=randint(0,100)
-				chat_room.joinroom(rooms,chatroom,clientname,room_ref,join_id,connect)
-				print("joined chatroom")
-			elif msg[:14] =="LEAVE_CHATROOM":
-				print("leaving chatroom")
-				msgs=msg.split("\n")
-				room_ref=int(msgs[0].split(":")[1])
-				join_id=int(msgs[1].split(":")[1])
-				client_name=msgs[2].split(":")[1]
-				chat_room.leaveroom(rooms,room_ref,join_id,client_name,connect)
-				print("left chatroom")
-			elif msg[:10] =="DISCONNECT":
-				print("client disconnecting")
-				msgs=msg.split("\n")
-				client_name=msgs[2].split(":")[1]
-				chat_room.disconnect(rooms,client_name,connect)
-				print("client disconnected")	
-				active=False
-			elif msg[:4] =="CHAT":
-				print("received message")
-				msgs=msg.split("\n")
-				room_ref=int(msgs[0].split(":")[1])
-				join_id=int(msgs[1].split(":")[1])
-				client_name=msgs[2].split(":")[1]
-				data=msgs[3].split(":")[1]
-				chat_room.chat(rooms,room_ref,join_id,client_name,data,connect)				
-				print("message received")
-			elif msg[:4]=="HELO":
-					print "HELO Received: "+msg
-					msg="%sIP:%s\nPort:%s\nStudentID:13319829\n"%(msg,str(gethostbyname(gethostname())),int(sys.argv[2]))
-					connect.sendall(msg)
-					print "HELO Sent"
-			elif msg[:12]=="KILL_SERVICE":				
-					connect.sendall("KILLED!!")
-					connect.shutdown(SHUT_RDWR)
-					connect.close()
-			else:
-				errmsg="ERROR_CODE:1\nERROR_DESCRIPTION: Parse error\n"
-				connect.sendall(errmsg)
-				
-		except Exception as error:	
-			errmsg="ERROR_CODE:2\nERROR_DESCRIPTION: %s\n"%(error.message)
+
+	try:
+		if msg[:13] == "JOIN_CHATROOM":
+			print("joining chatroom")
+			msgs=msg.split("\n")
+			chatroom=msgs[0].split(":")[1]
+			client_name=msgs[3].split(":")[1]
+			numb=0
+			for c in chatroom:
+				numb=numb+ord(c)
+			room_ref=numb%10
+			number=0
+			for c in client_name:
+				number=number+ord(c)
+			join_id=number%10
+			#chat_room.joinroom(rooms,chatroom,clientname,room_ref,join_id,connect)
+			retmsg="JOINED_CHATROOM:%s\nSERVER_IP:%s\nPORT:%s\nROOM_REF:%s\nJOIN_ID:%s\n"%(chatroom,sys.argv[1],int(sys.argv[2]),room_ref,join_id)
+			connect.sendall(retmsg)
+			print("joined chatroom")
+		elif msg[:14] =="LEAVE_CHATROOM":
+			print("leaving chatroom")
+			msgs=msg.split("\n")
+			room_ref=int(msgs[0].split(":")[1])
+			join_id=int(msgs[1].split(":")[1])
+			client_name=msgs[2].split(":")[1]
+			#chat_room.leaveroom(rooms,room_ref,join_id,client_name,connect)
+			retmsg="LEFT_CHATROOM:%s\nJOIN_ID:%s\n"%(str(room_ref),str(join_id))
+			connect.sendall(retmsg)
+			print("left chatroom")
+		elif msg[:10] =="DISCONNECT":
+			print("client disconnecting")
+			msgs=msg.split("\n")
+			client_name=msgs[2].split(":")[1]
+			number=0
+			for c in client_name:
+				number=number+ord(c)
+			join_id=number%10
+			#chat_room.disconnect(rooms,join_id,client_name,connect)
+			retmsg="DISCONNECT: 0\nPORT: 0\nCLIENT_NAME: Name\n"
+			connect.sendall(retmsg)
+			print("client disconnected")	
+		elif msg[:4] =="CHAT":
+			print("received message")
+			msgs=msg.split("\n")
+			room_ref=int(msgs[0].split(":")[1])
+			join_id=int(msgs[1].split(":")[1])
+			client_name=msgs[2].split(":")[1]
+			data=msgs[3].split(":")[1]
+			#chat_room.chat(rooms,room_ref,join_id,client_name,data,connect)	
+			retmsg="CHAT:%s\nCLIENT_NAME:%s\nMESSAGE:%s\n\n"%(str(room_ref),str(client_name),data)
+			connect.sendall(retmsg)
+			print("message received")
+		elif msg[:4]=="HELO":
+				print "HELO Received: "+msg
+				msg="%sIP:%s\nPort:%s\nStudentID:13319829\n"%(msg,str(gethostbyname(gethostname())),int(sys.argv[2]))
+				connect.sendall(msg)
+				print "HELO Sent"
+		elif msg[:12]=="KILL_SERVICE":				
+				connect.sendall("KILLED!!")
+				connect.shutdown(SHUT_RDWR)
+				connect.close()
+		else:
+			errmsg="ERROR_CODE:1\nERROR_DESCRIPTION: Parse error\n"
 			connect.sendall(errmsg)
+			
+	except Exception as error:	
+		errmsg="ERROR_CODE:2\nERROR_DESCRIPTION: %s\n"%(error.message)
+		connect.sendall(errmsg)
 
 
 def chat_server(hostname,port_number,numb_of_threads):
