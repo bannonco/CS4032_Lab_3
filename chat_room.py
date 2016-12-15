c088f1c61069c06b52f03ecebde106b504c95d16
bannonco <bannonco@tcd.ie>
2016-12-15 14:40:43 +0000

    fixed server and chatroom

--- chat_room.py
+++ chat_room.py
@@ -1,6 +1,7 @@
-from socket import *
-from Queue import Queue
-import threading 
+def broadcast(msg,connect,chatrooms,room_ref,join_id):
+	for join_id,connect in chatrooms[room_ref].iteritems():
+		connect.sendall(msg)
+
 
 class ChatRoom:
 
@@ -9,27 +10,32 @@ class ChatRoom:
 		self.portnum=portnum
 		
 	def joinroom(self,chatrooms,roomname,clientname,roomref,join_id,connect):
-		if room_ref not in chatrooms:
+		if roomref not in chatrooms:
 			
-			chatrooms[room_ref]={}
-		if join_id not in chatrooms[room_ref]:
+			chatrooms[roomref]={}
+		if join_id not in chatrooms[roomref]:
 			print("here")
-			chatrooms[room_ref][join_id]=connect
+			chatrooms[roomref][join_id]=connect
 			retmsg="JOINED_CHATROOM:%s\nSERVER_IP:%s\nPORT:%s\nROOM_REF:%s\nJOIN_ID:%s\n"%(roomname,self.hostname,self.portnum,roomref,join_id)
 			connect.sendall(retmsg)
+			broadmsg="CHAT:%s\nCLIENT_NAME:%s\nMESSAGE:%s has joined the chat\n\n"%(str(roomref),str(clientname),str(clientname))
+			broadcast(broadmsg,connect,chatrooms,roomref,join_id)
 	
 	def leaveroom(self,chatrooms,room_ref,join_id,clientname,connect):
 	
-		retmsg="LEFT_CHATROOM:%s\nJOIN_ID:%s\n"%(str(roomref),str(join_id))
+		retmsg="LEFT_CHATROOM:%s\nJOIN_ID:%s\n"%(str(room_ref),str(join_id))
 		connect.sendall(retmsg)
+		broadmsg="CHAT:%s\nCLIENT_NAME:%s\nMESSAGE:%s has left the chat\n\n"%(str(room_ref),str(clientname),str(clientname))
+		broadcast(broadmsg,connect,chatrooms,room_ref,join_id)
 		del chatrooms[room_ref][join_id]
 	
-	def disconnect(self,chatrooms,join_id,clientname,connect):
+	def disconnect(self,chatrooms,join_id,clientname,connect,active):
 		for room in chatrooms.keys():
 			if join_id in chatrooms[join_id]:
 				retmsg="DISCONNECT: 0\nPORT: 0\nCLIENT_NAME: Name\n"
 				connect.sendall(retmsg)
 				del chatrooms[room][join_id]
+		active=False
 	def chat(self,chatrooms,room_ref,join_id,clientname,msg,connect):
-		retmsg="CHAT:%s\nCLIENT_NAME:%s\nMESSAGE:%s\n\n"%(str(room_ref),str(clientname),msg)
-		connect.sendall(retmsg)
+		broadmsg="CHAT:%s\nCLIENT_NAME:%s\nMESSAGE:%s\n\n"%(str(room_ref),str(clientname),msg)
+		broadcast(broadmsg,connect,chatrooms,room_ref,join_id)
--- server.py
+++ server.py
@@ -1,17 +1,28 @@
-import sys
+import sys,os
 from socket import *
 from Queue import Queue
 from threading import Thread
 import collections
-#from chat_room import ChatRoom
+from chat_room import ChatRoom
 
-#chat_room=ChatRoom(sys.argv[1],int(sys.argv[2]))
-#rooms=collections.OrderedDict()
+chat_room=ChatRoom(sys.argv[1],int(sys.argv[2]))
+rooms=collections.OrderedDict()
+threads=[]
+activE=True
+threadLimit=100
+numb_of_threads=0
 
 
-def messages(connect,msg):
+hostname=sys.argv[1]
+port_number=int(sys.argv[2])
+sock=socket(AF_INET,SOCK_STREAM)
+sock.setsockopt(SOL_SOCKET,SO_REUSEADDR,1)
+sock.bind((hostname,port_number))
+sock.listen(10)
 
-	try:
+def messages(connect):
+	while activE:
+		msg=connect.recv(2048)
 		if msg[:13] == "JOIN_CHATROOM":
 			print("joining chatroom")
 			msgs=msg.split("\n")
@@ -25,9 +36,9 @@ def messages(connect,msg):
 			for c in client_name:
 				number=number+ord(c)
 			join_id=number%10
-			#chat_room.joinroom(rooms,chatroom,clientname,room_ref,join_id,connect)
-			retmsg="JOINED_CHATROOM:%s\nSERVER_IP:%s\nPORT:%s\nROOM_REF:%s\nJOIN_ID:%s\n"%(chatroom,sys.argv[1],int(sys.argv[2]),room_ref,join_id)
-			connect.sendall(retmsg)
+			chat_room.joinroom(rooms,chatroom,client_name,room_ref,join_id,connect)
+			#retmsg="JOINED_CHATROOM:%s\nSERVER_IP:%s\nPORT:%s\nROOM_REF:%s\nJOIN_ID:%s\n"%(chatroom,sys.argv[1],int(sys.argv[2]),room_ref,join_id)
+			#connect.sendall(retmsg)
 			print("joined chatroom")
 		elif msg[:14] =="LEAVE_CHATROOM":
 			print("leaving chatroom")
@@ -35,9 +46,9 @@ def messages(connect,msg):
 			room_ref=int(msgs[0].split(":")[1])
 			join_id=int(msgs[1].split(":")[1])
 			client_name=msgs[2].split(":")[1]
-			#chat_room.leaveroom(rooms,room_ref,join_id,client_name,connect)
-			retmsg="LEFT_CHATROOM:%s\nJOIN_ID:%s\n"%(str(room_ref),str(join_id))
-			connect.sendall(retmsg)
+			chat_room.leaveroom(rooms,room_ref,join_id,client_name,connect)
+			#retmsg="LEFT_CHATROOM:%s\nJOIN_ID:%s\n"%(str(room_ref),str(join_id))
+			#connect.sendall(retmsg)
 			print("left chatroom")
 		elif msg[:10] =="DISCONNECT":
 			print("client disconnecting")
@@ -47,9 +58,9 @@ def messages(connect,msg):
 			for c in client_name:
 				number=number+ord(c)
 			join_id=number%10
-			#chat_room.disconnect(rooms,join_id,client_name,connect)
-			retmsg="DISCONNECT: 0\nPORT: 0\nCLIENT_NAME: Name\n"
-			connect.sendall(retmsg)
+			chat_room.disconnect(rooms,join_id,client_name,connect,activE)
+			#retmsg="DISCONNECT: 0\nPORT: 0\nCLIENT_NAME: Name\n"
+			#connect.sendall(retmsg)
 			print("client disconnected")	
 		elif msg[:4] =="CHAT":
 			print("received message")
@@ -58,9 +69,9 @@ def messages(connect,msg):
 			join_id=int(msgs[1].split(":")[1])
 			client_name=msgs[2].split(":")[1]
 			data=msgs[3].split(":")[1]
-			#chat_room.chat(rooms,room_ref,join_id,client_name,data,connect)	
-			retmsg="CHAT:%s\nCLIENT_NAME:%s\nMESSAGE:%s\n\n"%(str(room_ref),str(client_name),data)
-			connect.sendall(retmsg)
+			chat_room.chat(rooms,room_ref,join_id,client_name,data,connect)	
+			#retmsg="CHAT:%s\nCLIENT_NAME:%s\nMESSAGE:%s\n\n"%(str(room_ref),str(client_name),data)
+			#connect.sendall(retmsg)
 			print("message received")
 		elif msg[:4]=="HELO":
 				print "HELO Received: "+msg
@@ -68,50 +79,21 @@ def messages(connect,msg):
 				connect.sendall(msg)
 				print "HELO Sent"
 		elif msg[:12]=="KILL_SERVICE":				
-				connect.sendall("KILLED!!")
-				connect.shutdown(SHUT_RDWR)
-				connect.close()
+				os._exit(1)
 		else:
 			errmsg="ERROR_CODE:1\nERROR_DESCRIPTION: Parse error\n"
 			connect.sendall(errmsg)
-			
-	except Exception as error:	
-		errmsg="ERROR_CODE:2\nERROR_DESCRIPTION: %s\n"%(error.message)
-		connect.sendall(errmsg)
+		
 
 
-def chat_server(hostname,port_number,numb_of_threads):
-	sock=socket(AF_INET,SOCK_STREAM)
-	sock.setsockopt(SOL_SOCKET,SO_REUSEADDR,1)
-	sock.bind((hostname,port_number))
-	sock.listen(10)
-	pool=Queue(numb_of_threads)
-	threads=[]
-
-	def handle(pool):
-		while True:
-			connect,address=pool.get()
-			msg=connect.recv(2048)
-			messages(connect,msg)
-
-	try:
-		for x in range(numb_of_threads):
-			thread=Thread(target=handle,args=(pool,))
-			thread.daemon=True
-			thread.start()
-			threads.append(thread)
-
-		while True:
-			connect,address=sock.accept()
-			try:
-				pool.put((connect,address),False)
-			except Queue.Full:
-				print("Queue full")
-		sock.close()
-	except(KeyboardInterrupt,SystemExit):
-		sock.close()
-		sys.exit(0)	
-
-
-if __name__ == "__main__":
-	chat_server(sys.argv[1],int(sys.argv[2]),int(sys.argv[3]))
+while activE:
+	if numb_of_threads<threadLimit:
+		connect,address=sock.accept()
+		threads.append(Thread(target=messages,args=(connect,)))		
+		threads[numb_of_threads].start()
+		global numb_of_threads
+		numb_of_threads=numb_of_threads+1
+	else:
+		print("max threads reached")
+#if __name__ == "__main__":
+#	chat_server(sys.argv[1],int(sys.argv[2]),int(sys.argv[3]))
