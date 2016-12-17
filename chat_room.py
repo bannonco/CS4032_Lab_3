9c81f37e1d93302d3ad559c372d2bc13bf9edac6
bannonco <bannonco@tcd.ie>
2016-12-17 15:24:20 +0000

    fixed disconnect

--- chat_room.py
+++ chat_room.py
@@ -10,11 +10,9 @@ class ChatRoom:
 		self.portnum=portnum
 		
 	def joinroom(self,chatrooms,roomname,clientname,roomref,join_id,connect):
-		if roomref not in chatrooms:
-			
+		if roomref not in chatrooms:			
 			chatrooms[roomref]={}
 		if join_id not in chatrooms[roomref]:
-			print("here")
 			chatrooms[roomref][join_id]=connect
 			retmsg="JOINED_CHATROOM:%s\nSERVER_IP:%s\nPORT:%s\nROOM_REF:%s\nJOIN_ID:%s\n"%(roomname,self.hostname,self.portnum,roomref,join_id)
 			connect.sendall(retmsg)
@@ -30,11 +28,13 @@ class ChatRoom:
 		del chatrooms[room_ref][join_id]
 	
 	def disconnect(self,chatrooms,join_id,clientname,connect,active):
-		for room in chatrooms.keys():
-			if join_id in chatrooms[join_id]:
-				retmsg="DISCONNECT: 0\nPORT: 0\nCLIENT_NAME: Name\n"
-				connect.sendall(retmsg)
-				del chatrooms[room][join_id]
+		for room_ref in chatrooms.keys():
+			if join_id in chatrooms[room_ref]:
+				#retmsg="DISCONNECT: 0\nPORT: 0\nCLIENT_NAME: Name\n"
+				#connect.sendall(retmsg)
+				broadmsg="CHAT:%s\nCLIENT_NAME:%s\nMESSAGE:%s has disconnected from the chat\n\n"%(str(room_ref),str(clientname),str(clientname))
+				broadcast(broadmsg,connect,chatrooms,room_ref,join_id)
+				del chatrooms[room_ref][join_id]
 		active=False
 	def chat(self,chatrooms,room_ref,join_id,clientname,msg,connect):
 		broadmsg="CHAT:%s\nCLIENT_NAME:%s\nMESSAGE:%s\n\n"%(str(room_ref),str(clientname),msg)
